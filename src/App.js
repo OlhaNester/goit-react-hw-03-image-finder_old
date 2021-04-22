84b03ae1ac3bc338a6dc3946145e0371dbab3ddb
OlhaNester <olga_nv@ukr.net>
2021-04-22 17:48:32 +0300

    модалка

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import React, { Component } from 'react';
 
 import Searchbar from './components/Searchbar';
 import ImageGallery from './components/ImageGallery';
+import Modal from './components/Modal';
 
 //import Button from './components/Button';
-//import Loader from './components/Loader';
+//import Loader from './components/Loader'
 //import './App.css';
 
 class App extends Component {
@@ -21,12 +22,30 @@ class App extends Component {
   };
 
   componentDidMount() {}
+
+  toggleModal = () => {
+    this.setState(({ showModal }) => ({ showModal: !showModal }));
+  };
+
   render() {
-    const { images } = this.state;
+    const { images, showModal } = this.state;
     return (
       <div className="App">
         <Searchbar onSubmit={this.handleSearch} />
         <ImageGallery images={images} />
+        <button type="button" onClick={this.toggleModal}>
+          {' '}
+          Открыть модалку
+        </button>
+        {showModal && (
+          <Modal onClose={this.toggleModal}>
+            <h1>Hello!</h1>
+            <button type="button" onClick={this.toggleModal}>
+              {' '}
+              Закрыть модалку
+            </button>
+          </Modal>
+        )}
         {/* <Button>
           <Loader />
         </Button> */}
--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,21 +1,40 @@
 import React, { Component } from 'react';
+import { createPortal } from 'react-dom';
 import styles from './Modal.module.css';
 //import PropTypes from 'prop-types';
+
+const modalRoot = document.querySelector('#modal-root');
 
 export default class Modal extends Component {
   componentDidMount() {
     console.log('Modal componentDidMount');
+    window.addEventListener('keydown', this.handleKeyDown);
   }
   componentWillUnmount() {
     console.log('Modal componentWillUnmount');
+    window.removeEventListener('keydown', this.handleKeyDown);
   }
+
+  handleKeyDown = e => {
+    if (e.code === 'Escape') {
+      this.props.onClose();
+    }
+  };
+
+  handleOverlayClick = event => {
+    if (event.currentTarget === event.target) {
+      this.props.onClose();
+    }
+  };
   render() {
-    return (
-      <div className={styles.Overlay}>
+    return createPortal(
+      <div className={styles.Overlay} onClick={this.handleOverlayClick}>
         <div className={styles.Modal}>
-          <img src="" alt="" />
+          {this.props.children}
+          {/* <img src="" alt="" /> */}
         </div>
-      </div>
+      </div>,
+      modalRoot,
     );
   }
 }
