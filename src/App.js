c0c4c4bac4b128c3bd78a65431d62f7e89fb40c0
OlhaNester <olga_nv@ukr.net>
2021-04-24 22:48:59 +0300

    999

--- src/App.js
+++ src/App.js
@@ -1,39 +1,73 @@
 import React, { Component } from 'react';
 
 import Searchbar from './components/Searchbar';
+import apiImage from './services/apiImage';
+
 import ImageGallery from './components/ImageGallery';
 import Modal from './components/Modal';
 
-//import Button from './components/Button';
-//import Loader from './components/Loader'
+import Button from './components/Button';
+import Loader from './components/Loader';
+
 //import './App.css';
 
 class App extends Component {
   state = {
     images: [],
+    page: 1,
     filter: '',
-    showModal: false,
-    //isLoading: false,
+    // showModal: false,
+    // isLoading: false,
     //error: null,
   };
+  // проерка, что в стэйт уже новое значение, пора делать запрос
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.filter !== this.state.filter) {
+      this.fetchImage();
+    }
+  }
 
   handleSearch = query => {
-    this.setState({ filter: query });
+    this.setState({ filter: query, page: 1, images: [] });
   };
-
-  componentDidMount() {}
+  fetchImage = () => {
+    const { page, filter } = this.state;
+    const options = {
+      filter,
+      page,
+    };
+    apiImage.fetchImage(options).then(hits => {
+      this.setState(prevState => ({
+        images: [...prevState.images, ...hits],
+        page: prevState.page + 1,
+      }));
+    });
+  };
 
   toggleModal = () => {
     this.setState(({ showModal }) => ({ showModal: !showModal }));
   };
 
   render() {
-    const { images, showModal } = this.state;
+    const { images, showModal, isLoading } = this.state;
     return (
       <div className="App">
         <Searchbar onSubmit={this.handleSearch} />
+        {/* <ul>
+          {' '}
+          {images.map(({ id, webformatURL, tags }) => (
+            <li key={id}>
+              <img src={webformatURL} alt={tags} />
+            </li>
+          ))}
+        </ul>
+        <button type="button" onClick={this.fetchImage}>
+          {' '}
+          Загрузить еще
+        </button> */}
+
         <ImageGallery images={images} />
-        <button type="button" onClick={this.toggleModal}>
+        {/* <button type="button" onClick={this.toggleModal}>
           {' '}
           Открыть модалку
         </button>
@@ -46,9 +80,8 @@ class App extends Component {
             </button>
           </Modal>
         )}
-        {/* <Button>
-          <Loader />
-        </Button> */}
+        {isLoading && <Loader />}
+        {images.length > 0 && !isLoading && <Button />}   */}
       </div>
     );
   }
---
+++ src/services/apiImage.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+// const apiKey = '9657010-5b32a47b9f15e54eb73ad95a0';
+// const baseURL = 'https://pixabay.com/api/';
+const fetchImage = ({ filter = '', page = 1 }) => {
+  return axios
+    .get(
+      `https://pixabay.com/api/?q=${filter}&page=${page}&key=9657010-5b32a47b9f15e54eb73ad95a0&image_type=photo&orientation=horizontal&per_page=12`,
+    )
+    .then(response => response.data.hits);
+};
+export default { fetchImage };
